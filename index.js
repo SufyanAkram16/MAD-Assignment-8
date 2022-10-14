e3bb8d8751def6ea270416b7351f04ad9f8efe27
sufyanakram16 <sufyanakram1223@gmail.com>
2022-10-14 05:27:47 +0500

    4 programs

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 // 1. Write a ts program to read and print elements of array.
-
-
 // var array:string [] = ["a","ab","abc", "abcd"];
-
 // array.forEach(Element => {
 //     console.log(Element);
 // });
-
 // 2. Write a ts program to print all negative elements in an array.
-
 // const negElements = (a:number[]) => {
 //     const negArray:number[] = [];
 //     for( let i=0; i< a.length; i++) {
@@ -16,58 +11,39 @@
 //             negArray.push(a[i]);
 //         }
 //     }
-
 //     negArray.forEach(Element => {
 //         console.log(Element);
 //     })
 // } 
-
 // negElements([12, 13, -78, -7, -9 , 12, -9])
-
 // 3. Write a ts program to find sum of all array elements. 
-
-
 // const sumArray = (x:number[]) => {
 //     let sum = 0;
 //     for( let i=0; i<x.length; i++){
 //         sum += x[i];
 //     }
-
 //     console.log(sum);
 // }
-
 // sumArray([1,2,15,15,14,18,19]);
-
-
-
 // 4. Write a ts program to find maximum and minimum element in an array.
-
 // const maxMinNum = (x:number[]) => {
 //     let min:number = x[0];
 //     let max:number = x[0];
-
 //     for(let i=1; i<x.length; i++){
 //         if(x[i] < min) {
 //             min = x[i];
 //         }
-
 //     }
-
 //     for(let i=1; i<x.length; i++){
 //         if(x[i] > max) {
 //             max = x[i];
 //         }
 //     }
-
 //     console.log("Minimum number is:" + min);
 //     console.log("Maximum number is:" + max);
 // }
-
 // maxMinNum([1,2,3,4,5,6,7,8,9]);
-
 // 5. Write a ts program to find second largest element in an array.
-
-
 // 6. Write a ts program to count total number of even and odd elements in an array.
 // 7. Write a ts program to count total number of negative elements in an array.
 // 8. Write a ts program to copy all elements from an array to another array.
